88577c6e6bd335c9137ffd714355deced9415360
mdolbnin <mdolbnin@ozon.ru>
2021-12-01 13:30:18 +0300

    fix richman

--- ejudge/md3/richman.py
+++ ejudge/md3/richman.py
@@ -1,12 +1,12 @@
 def rich_bitch_game(cash):
-    num = 0
+    result = []
     while cash != 0: # идем в обратном порядке операций до 0
         if cash % 2 == 0: # операция dbl
-            num += 1
+            result.append('dbl')
             cash //= 2
 
         if cash == 1: # операция inc
-            num += 1
+            result.append('inc')
             cash -= 1
 
         if cash % 2 == 1:
@@ -14,6 +14,7 @@ def rich_bitch_game(cash):
             way_second = bin(cash - 1)[2:]
             
             if way_first.count('1') < way_second.count('1'): # операция dec
+                result.append('dec')
                 cash += 1
 
             elif way_first.count('1') == way_second.count('1'):
@@ -22,16 +23,21 @@ def rich_bitch_game(cash):
 
                 for i in range(length, len(way_first) - 1):
                     if way_first[i] > way_second[i]: # операция dec
+                        result.append('dec')
                         cash += 1
                         break
                     if way_first[i] < way_second[i] or len(way_second) < len(way_first): # операция inc
+                        result.append('inc')
                         cash -= 1
                         break
             else: # операция inc
+                result.append('inc')
                 cash -= 1
-            num += 1
-    return num
+    return result
             
 if __name__=="__main__":
     cash = int(input())
-    print(rich_bitch_game(cash))
+    res = rich_bitch_game(cash)[::-1]
+    #for command in res:
+    #    print(command)
+    print(len(res))
